2a10c053498227e1c6444f10b315671d87768e2f
alangui <2516394328@qq.com>
2018-10-09 16:06:16 +0800

    create new project

---
+++ src/main/java/com/qing/niu/design/pattern/Application.java
@@ -0,0 +1,36 @@
+package com.qing.niu.design.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.FallbackWebSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SpringBootWebSecurityConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * <p>
+ *     SpringBoot启动入口
+ * </p>
+ *
+ * @author huqingniu
+ * @version 1.0.0
+ * @date 2018/10/9
+ */
+@SpringBootApplication(exclude = {SpringDataWebAutoConfiguration.class,
+        FallbackWebSecurityAutoConfiguration.class, SpringBootWebSecurityConfiguration.class})
+@ImportResource({"classpath:spring/application-context.xml"})
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Application.class);
+        app.setShowBanner(false);
+        app.run(args);
+    }
+}
